e8e7f98bc626160d7dec22e8d8fff1e08bc6c471
wideprism <winsvintus@gmail.com>
2024-10-29 15:11:05 +0100

    Fixed the random numbers ambiguity (removed the extra generator in Simulation method), tabulation interface rework

--- MnS_L2_Dog-Flea/Program.cs
+++ MnS_L2_Dog-Flea/Program.cs
@@ -7,7 +7,7 @@
 		private static readonly Random random = new();
 
 		// Simulates one iteration of the model
-		static (int, int) Transition((int, int) state)
+		static (int, (int,int)) Transition((int, int) state)
 		{
 			int fleasOnDogA = state.Item1;
 			int fleasOnDogB = state.Item2;
@@ -25,47 +25,43 @@
 				fleasOnDogB--;
 			}
 
-			return (fleasOnDogA, fleasOnDogB);
+			return (randomFlea, (fleasOnDogA, fleasOnDogB));
 		}
 
-		// Runs a single simulation for specific amount of iterations
-		static int RunSimulation(int n, int iterations)
+        // Runs a single simulation for specific amount of iterations
+        static long RunSimulation(int n, long iterations, bool mode)
+        {
+            (int, int) state = (n, 0); // Initialize initial state of X(0)
+			long returnIteration = 0;
+			if (mode) { Console.WriteLine("i\tx(i-1)\t\tRandom Number\t\tx(i)"); }
+
+            for (int i = 1; i <= iterations; i++)
+            {
+				if (mode) { Console.Write($"{i}\t({state.Item1}, {state.Item2})\t\t"); }
+				int randomNumber;
+				(randomNumber, state) = Transition(state);
+				if (mode) { Console.Write($"{randomNumber}\t\t({state.Item1}, {state.Item2})\n"); }
+				//else { Console.Write(i); Console.SetCursorPosition(Console.CursorLeft - i.ToString().Length, Console.CursorTop); }
+				if (state == (n,0)) { returnIteration = i; break; }
+            }
+			return returnIteration;
+        }
+
+        // Runs 20 simulations and averages the return times
+        static (double, double) CalculateAverageReturnToInitialState(int n, int simulations, long iterations, bool mode)
 		{
-			(int, int) state = (n, 0); // Initial state X(0) = (20,0)
-			int returnIteration = 0;
-			Console.WriteLine("i\tRandom Number\tx(i)");
-
-			for (int i = 1; i <= iterations; i++)
-			{
-				int randomNumber = random.Next(n); // Random number for transition
-				state = Transition(state);
-				Console.WriteLine($"{i}\t{randomNumber}\t\t({state.Item1}, {state.Item2})");
-				if (state == (n, 0))
-				{
-					Console.WriteLine($"The model has returned to its initial state at iteration {i}.");
-					returnIteration = i;
-					break;
-				}
-			}
-
-			if (state == (n, 0)) return returnIteration; else return 0;
-		}
-
-		// Runs 20 simulations and averages the return times
-		static (double, double) CalculateAverageReturnToInitialState(int n, int simulations, int iterations)
-		{
-			int totalIterations = 0;
-			int simulationsToReturn = 0;
+			long totalIterations = 0;
+			long iterationsToReturn = 0;
 
 			for (int i = 0; i < simulations; i++)
 			{
-				Console.WriteLine($"\nSimulation {i + 1}:");
-				int iterationsToReturn = RunSimulation(n, iterations);
-                simulationsToReturn += iterationsToReturn > 0 ? 1 : 0;
+				Console.WriteLine($"\nSimulation {i + 1}: ");
+				iterationsToReturn = RunSimulation(n, iterations, mode);
 				totalIterations += iterationsToReturn;
+				Console.Write(iterationsToReturn);
 			}
 
-			return (simulationsToReturn, (double)totalIterations / simulations);
+			return (iterationsToReturn, (double)totalIterations / simulations);
 		}
 
 		static void Main()
@@ -75,9 +71,9 @@
 			int iterations = 20;
 
 			Console.WriteLine("Running 20 simulations of the Dog-Flea Model:");
-			(double, double) averageReturn = CalculateAverageReturnToInitialState(n, simulations, iterations);
-			Console.WriteLine($"\nValue of simulations when the model has returned to initial state: {averageReturn.Item1}");
-			Console.WriteLine($"Average value of iterations to return to initial state: {averageReturn.Item2}");
+			RunSimulation(n, iterations, true);
+            (double, double) averageReturn = CalculateAverageReturnToInitialState(n, simulations, long.MaxValue, false);
+            Console.WriteLine($"\nAverage value of iterations to return to initial state: {averageReturn.Item2}");
 		}
 	}
 
